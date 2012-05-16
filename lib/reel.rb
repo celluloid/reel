4209c3daf167cbb655712224f89bb61cd11d9345
Sebastian Edwards <s.thursday@gmail.com>
2012-05-16 19:13:44 +1200

    Adding support for streaming responses.

--- lib/reel.rb
+++ lib/reel.rb
@@ -9,6 +9,8 @@ require 'reel/logger'
 require 'reel/request'
 require 'reel/request/parser'
 require 'reel/response'
+require 'reel/response/chunked_body'
+require 'reel/streaming_response'
 require 'reel/server'
 
 # A Reel good HTTP server
