41b034d857a5cd54316b6080767daedc187adbfb
Tony Arcieri <tony.arcieri@gmail.com>
2012-07-23 21:46:09 -0700

    Update hello world example

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -6,7 +6,13 @@ addr, port = '127.0.0.1', 1234
 
 puts "*** Starting server on #{addr}:#{port}"
 Reel::Server.new(addr, port) do |connection|
-  connection.respond :ok, "hello, world!"
-end
+  # To use keep-alive with Reel, use a while loop that repeatedly calls
+  # connection.request and consumes connection objects
+  while request = connection.request
+    # Ordinarily we'd route the request here, e.g.
+    # route request.url
+    connection.respond :ok, "hello, world!"
+  end
 
-sleep
+  # Reel takes care of closing the connection
+end
