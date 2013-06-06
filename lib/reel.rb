56841105945a3a5fbcdedcb12f35360fb3fd89f9
slivu <slivuz@gmail.com>
2013-06-06 07:01:22 +0300

    raise a RequestError when connection used after it was upgraded

--- lib/reel.rb
+++ lib/reel.rb
@@ -29,8 +29,15 @@ module Reel
   # Error reading a request
   class RequestError < StandardError; end
 
-  # Error occured performing IO on a socket
+  # Error occurred performing IO on a socket
   class SocketError < RequestError; end
+
+  # Error occurred when trying to use the socket after it was upgraded
+  class SocketUpgradedError < NilClass
+    def self.method_missing(m, *)
+      raise(Reel::RequestError, 'Reel::Connection#socket can not be used anymore as it was upgraded. Use Reel::WebSocket instance instead.')
+    end
+  end
 
   # Error occured during a WebSockets handshake
   class HandshakeError < RequestError; end
