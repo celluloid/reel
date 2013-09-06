1ee75d3207e7a05b116d74809a29587d12cfecb4
Tony Arcieri <tony.arcieri@gmail.com>
2013-09-06 00:12:14 -0700

    Remove Reel::RequestInfo#method

--- lib/reel/request_info.rb
+++ lib/reel/request_info.rb
@@ -12,14 +12,6 @@ module Reel
     UPGRADE   = 'Upgrade'.freeze
     WEBSOCKET = 'websocket'.freeze
 
-    # Array#include? seems slow compared to Hash lookup
-    request_methods = Http::METHODS.map { |m| m.to_s.upcase }
-    REQUEST_METHODS = Hash[request_methods.zip(request_methods)].freeze
-
-    def method
-      REQUEST_METHODS[http_method]
-    end
-
     def websocket_request?
       headers[UPGRADE] && headers[UPGRADE].downcase == WEBSOCKET
     end
