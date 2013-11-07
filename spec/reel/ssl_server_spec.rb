bbcc1eb84972cc57b48ab74ddea026f0d0331d02
Stephen Touset <stephen@squareup.com>
2013-11-07 15:38:07 -0800

    Test with signed client certificates

--- spec/reel/ssl_server_spec.rb
+++ spec/reel/ssl_server_spec.rb
@@ -7,8 +7,12 @@ describe Reel::SSLServer do
   let(:endpoint)         { URI(example_url) }
   let(:response_body)    { "ohai thar" }
 
+  let(:ca_file) { fixture_dir.join('ca.crt').to_s }
+
   let(:server_cert) { fixture_dir.join("server.crt").read }
   let(:server_key)  { fixture_dir.join("server.key").read }
+  let(:client_cert) { fixture_dir.join("client.crt").read }
+  let(:client_key)  { fixture_dir.join("client.key").read }
 
   it "receives HTTP requests and sends responses" do
     ex = nil
@@ -40,11 +44,44 @@ describe Reel::SSLServer do
     raise ex if ex
   end
 
-  def with_reel_sslserver(handler, context = OpenSSL::SSL::SSLContext.new)
+  it 'verifies client SSL certs when provided with a CA' do
+    ex = nil
+
+    handler = proc do |connection|
+      begin
+        request = connection.request
+        request.method.should eq 'GET'
+        request.version.should eq '1.1'
+        request.url.should eq example_path
+
+        connection.respond :ok, response_body
+      rescue => ex
+      end
+    end
+
+    with_reel_sslserver(handler, :ca_file => self.ca_file) do
+      http         = Net::HTTP.new(endpoint.host, endpoint.port)
+      http.use_ssl = true
+      http.cert    = OpenSSL::X509::Certificate.new self.client_cert
+      http.key     = OpenSSL::PKey::RSA.new         self.client_key
+
+      # FIXME: VERIFY_NONE is bad! Authenticate the server cert!
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request  = Net::HTTP::Get.new(endpoint.path)
+      response = http.request(request)
+
+      response.body.should eq response_body
+    end
+
+    raise ex if ex
+  end
+
+  def with_reel_sslserver(handler, options = {})
     options = {
       :cert => server_cert,
       :key  => server_key
-    }
+    }.merge(options)
 
     server = Reel::SSLServer.new(example_addr, example_ssl_port, options, &handler)
     yield server
