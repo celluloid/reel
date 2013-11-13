169a7fd18fb633f69f0208dd4dec083cc3025b6e
//de <de@01e.mobi>
2013-11-12 16:54:28 -0800

    Add SocketMixin for de/optimization per-server.

--- lib/reel/mixins.rb
+++ lib/reel/mixins.rb
@@ -19,6 +19,7 @@ module Reel
       # NOTE: Celluloid::IO does not yet support non-blocking reverse DNS
       socket.peeraddr(true)[2]
     end
+
   end
 
   module RequestMixin
@@ -57,6 +58,43 @@ module Reel
 
     def fragment
       uri.fragment
+    end
+    
+  end
+
+  module SocketMixin
+
+    if RUBY_PLATFORM =~ /linux/
+      # Only Linux supports the mix of socket behaviors given in these optimizations.
+      # Beaware, certain optimizations may work individually off Linux; not together.
+      def optimize_socket socket
+        if TCPSocket === socket
+          socket.setsockopt( Socket::IPPROTO_TCP, :TCP_NODELAY, 1 )
+          socket.setsockopt( Socket::IPPROTO_TCP, 3, 1 ) # TCP_CORK
+          socket.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1 )
+        end
+      end
+
+      def deoptimize_socket socket
+        if TCPSocket === socket
+          socket.setsockopt( Socket::IPPROTO_TCP, :TCP_NODELAY, 1 )
+          socket.setsockopt( Socket::IPPROTO_TCP, 3, 1 ) # TCP_CORK
+          socket.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1 )
+        end
+      end
+    else
+      # If the underying OS is not Linux, apply the remaining available optimizations.
+      def optimize_socket socket
+        if TCPSocket === socket
+          socket.setsockopt( Socket::IPPROTO_TCP, :TCP_NODELAY, 1 )
+        end
+      end
+
+      def deoptimize_socket socket
+        if TCPSocket === socket
+          socket.setsockopt( Socket::IPPROTO_TCP, :TCP_NODELAY, 0 )
+        end
+      end
     end
 
   end
