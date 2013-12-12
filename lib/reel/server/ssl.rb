37dd5fc66ce66999c21191a8e2e8a57a74fb4765
//de <de@01e.mobi>
2013-12-11 16:05:56 -0800

    tweaks I did not save in my editor before commit

--- lib/reel/server/ssl.rb
+++ lib/reel/server/ssl.rb
@@ -16,9 +16,9 @@ module Reel
         # Ideally we can encapsulate this rather than making Ruby OpenSSL a
         # mandatory part of the Reel API. It would be nice to support
         # alternatives (e.g. Puma's MiniSSL)
-        ssl_context         = OpenSSL::SSL::SSLContext.new
-        ssl_context.cert    = OpenSSL::X509::Certificate.new options.fetch(:cert)
-        ssl_context.key     = OpenSSL::PKey::RSA.new options.fetch(:key)
+        ssl_context = OpenSSL::SSL::SSLContext.new
+        ssl_context.cert = OpenSSL::X509::Certificate.new options.fetch(:cert)
+        ssl_context.key = OpenSSL::PKey::RSA.new options.fetch(:key)
 
         ssl_context.ca_file = options[:ca_file]
         ssl_context.ca_path = options[:ca_path]
@@ -26,10 +26,14 @@ module Reel
         # if verify_mode isn't explicitly set, verify peers if we've
         # been provided CA information that would enable us to do so
         ssl_context.verify_mode = case
-          when options.include?(:verify_mode) then options[:verify_mode]
-          when options.include?(:ca_file)     then OpenSSL::SSL::VERIFY_PEER
-          when options.include?(:ca_path)     then OpenSSL::SSL::VERIFY_PEER
-          else                                     OpenSSL::SSL::VERIFY_NONE
+        when options.include?(:verify_mode)
+          options[:verify_mode]
+        when options.include?(:ca_file)
+          OpenSSL::SSL::VERIFY_PEER
+        when options.include?(:ca_path)
+          OpenSSL::SSL::VERIFY_PEER
+        else
+          OpenSSL::SSL::VERIFY_NONE
         end
 
         optimize @tcpserver = Celluloid::IO::TCPServer.new(host, port)
