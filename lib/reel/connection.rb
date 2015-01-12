7e7dd9c1169a43a668bae28aff3445a2a7b7c6ed
Martin Skinner <martin@artcom.de>
2015-01-12 11:55:45 +0100

    Yosemite sometimes raises EPROTOTYPE when trying to write to a closing connection.

--- lib/reel/connection.rb
+++ lib/reel/connection.rb
@@ -120,7 +120,7 @@ module Reel
         @parser.reset
         @request_fsm.transition :closed
       end
-    rescue IOError, Errno::ECONNRESET, Errno::EPIPE, RequestError
+    rescue IOError, Errno::ECONNRESET, Errno::EPIPE, Errno::EPROTOTYPE, RequestError
       # The client disconnected early, or there is no request
       @keepalive = false
       @request_fsm.transition :closed
