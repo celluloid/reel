2b29273aabbd80f4469ee87aa49a7b49bea58c35
//de <de@01e.mobi>
2015-03-24 04:36:27 -0700

    travis seems not to like 'return' in a spec

--- spec/reel/unix_server_spec.rb
+++ spec/reel/unix_server_spec.rb
@@ -1,39 +1,41 @@
 require 'spec_helper'
 require 'net/http'
 
-return if defined? JRUBY_VERSION
+unless defined?(JRUBY_VERSION)
 
-RSpec.describe Reel::Server::UNIX do
-  let(:endpoint) { URI(example_url) }
-  let(:response_body) { "ohai thar" }
+  RSpec.describe Reel::Server::UNIX do
+    let(:endpoint) { URI(example_url) }
+    let(:response_body) { "ohai thar" }
 
-  it 'allows connections over UNIX sockets' do
-    ex = nil
+    it 'allows connections over UNIX sockets' do
+      ex = nil
 
-    handler = proc do |connection|
-      begin
-        request = connection.request
-        expect( request.method ).to eq 'GET'
-        connection.respond :ok, self.response_body
+      handler = proc do |connection|
+        begin
+          request = connection.request
+          expect( request.method ).to eq 'GET'
+          connection.respond :ok, self.response_body
+        end
       end
-    end
 
-    Dir::Tmpname.create('reel-sock') do |path|
-      begin
-        server  = Reel::Server::UNIX.new(path, &handler)
-        sock    = Net::BufferedIO.new Celluloid::IO::UNIXSocket.new(path)
-        request = Net::HTTP::Get.new('/')
+      Dir::Tmpname.create('reel-sock') do |path|
+        begin
+          server  = Reel::Server::UNIX.new(path, &handler)
+          sock    = Net::BufferedIO.new Celluloid::IO::UNIXSocket.new(path)
+          request = Net::HTTP::Get.new('/')
 
-        request.exec(sock, '1.1', path)
-        response = Net::HTTPResponse.read_new(sock)
-        response.reading_body(sock, request.response_body_permitted?) { }
+          request.exec(sock, '1.1', path)
+          response = Net::HTTPResponse.read_new(sock)
+          response.reading_body(sock, request.response_body_permitted?) { }
 
-        expect(response.body).to eq(self.response_body)
-      ensure
-        server.terminate if server && server.alive?
+          expect(response.body).to eq(self.response_body)
+        ensure
+          server.terminate if server && server.alive?
+        end
       end
-    end
 
-    raise ex if ex
+      raise ex if ex
+    end
   end
+  
 end
