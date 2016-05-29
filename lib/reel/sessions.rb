1fa7c99c6567342407d50268421d33b886f04f41
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-05-29 12:57:18 +0530

    initial structure to visulize working(need refactoring)

---
+++ lib/reel/sessions.rb
@@ -0,0 +1,54 @@
+module Reel
+  module Sessions
+
+    # Basic structure to visualize working of session handlers
+    # TODO
+
+
+    # default session configuration
+    DEFAULT_CONFIG = {
+      # secret_key: '',
+      # session_length: 0, #TODO
+      # session_name: 'reel_sessions'
+      # .....
+    }
+
+
+    module SessionMethods
+
+      # adding/changing confiuration
+      def configuration options={}
+        #TODO
+      end
+
+      # initializing session
+      def initialize_session req
+        # Extract out key from req cookie and search for uuid in our concurrent
+        # hash store if found associate value to @session
+        # else
+        # generate new key and associate empty hash to @session
+        # TODO
+      end
+
+      # to expose value hash
+      def session
+        @session
+      end
+
+      # finalizing the session
+      def finalize_session
+        # save the @session into concurrent hash and return its associated uuid
+        # set this uuid to response header
+        # TODO
+      end
+
+      # set cookie with uuid in response header
+      def set_response uuid
+        # encrypt uuid (data) and add Set_cookie into header with proper expiration
+        # TODO
+      end
+
+    end
+
+  end
+end
