a67f07514ee15adb03207d35c7eafe151765df01
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-07-09 20:43:10 +0530

    filled "multipart?"

--- lib/reel/request/multipart.rb
+++ lib/reel/request/multipart.rb
@@ -3,9 +3,22 @@ require 'multipart_parser/reader'
 
 module Reel
   class Request
+    CONTENT_TYPE = 'Content-Type'.freeze
 
     def multipart
-      # TODO
+      @multipart.decode
+    end
+
+    def multipart? body=nil
+      return @multipart.is_a? Reel::Request::Multipart if @multipart or body.nil?
+      boundary = extract_boundary self.headers[CONTENT_TYPE]
+      # initializing Multipart
+      @multipart = Reel::Request::Multipart.new body,boundary if boundary
+      @multipart.is_a? Reel::Request::Multipart
+    end
+
+    def extract_boundary content
+      MultipartParser::Reader.extract_boundary_value content
     end
 
     class Multipart
