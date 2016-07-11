722d5092b4f7083e53fd8b7b526bd3342f5df263
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-07-11 20:23:06 +0530

    refactoring as per code convention

--- lib/reel/request/multipart.rb
+++ lib/reel/request/multipart.rb
@@ -11,25 +11,25 @@ module Reel
       @multipart.decode if multipart? @body
     end
 
-    def multipart? body=nil
-      return @multipart.is_a? Reel::Request::Multipart if @multipart or body.nil?
+    def multipart?(body = nil)
+      return @multipart.is_a? Reel::Request::Multipart if @multipart || body.nil?
       boundary = extract_boundary self.headers[CONTENT_TYPE]
       # initializing Multipart
-      @multipart = Reel::Request::Multipart.new body,boundary if boundary
+      @multipart = Reel::Request::Multipart.new body, boundary if boundary
       @multipart.is_a? Reel::Request::Multipart
     rescue => e
       info e.to_s
       @multipart = false
     end
 
-    def extract_boundary content
+    def extract_boundary(content)
       MultipartParser::Reader.extract_boundary_value content
     end
 
     class Multipart
       extend Forwardable
 
-      def initialize(body,boundary)
+      def initialize(body, boundary)
         @files = {}
         @body = body
         @boundary = boundary
