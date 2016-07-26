3897c8b47f8c71f461ee5f11cd280517ea7971f3
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-07-26 16:28:26 +0530

    Added test to parse file containing semicolon in filename

--- spec/reel/multipart_spec.rb
+++ spec/reel/multipart_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Reel::Request::Multipart do
   let(:endpoint) { URI(example_url) }
   let(:response_body) { "ohai thar" }
   let(:txt_filepath){ 'spec/support/multipart_test_example.txt' }
+  let(:txt_semicolon_filepath){ 'spec/support/multipart_test_example;.txt' }
   let(:img_path){'logo.png'}
 
   EOL = "\r\n".freeze
@@ -179,4 +180,40 @@ RSpec.describe Reel::Request::Multipart do
 
       raise ex if ex
     end
+    it "Parses data if content is multipart type (filename with semicolons)" do
+    ex = nil
+
+    handler = proc do |connection|
+      begin
+        req = connection.request
+        expect(req.multipart? req.body).to eq true
+        expect(req.multipart.empty?).to eq false
+        expect(req.multipart[PART_NAME][:ended]).to eq true
+        expect(req.multipart[PART_NAME][:data]).to eq File.read(txt_semicolon_filepath)
+
+        req.respond :ok, response_body
+      rescue => ex
+      end
+    end
+
+    with_reel(handler) do
+
+      post_body = []
+      post_body << "--#{MULTIPART_BOUNDARY}#{EOL}"
+      post_body << "Content-Disposition: form-data; name=\"#{PART_NAME}\"; filename=\"#{File.basename(txt_semicolon_filepath)}\"#{EOL}"
+      post_body << "Content-Type: text/plain#{EOL}"
+      post_body << EOL
+      post_body << File.read(txt_semicolon_filepath)
+      post_body << "#{EOL}--#{MULTIPART_BOUNDARY}--#{EOL}"
+
+      http = Net::HTTP.new(endpoint.host, endpoint.port)
+      request = Net::HTTP::Post.new(endpoint.request_uri)
+      request.body = post_body.join
+      request["Content-Type"] = "multipart/form-data, boundary=#{MULTIPART_BOUNDARY}"
+
+      http.request(request)
+    end
+
+    raise ex if ex
+  end
 end
