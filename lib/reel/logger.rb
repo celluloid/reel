a0d17232f7f101a64a6a00aea999fef8ed14306d
kenichi nakamura <kenichi.nakamura@gmail.com>
2016-11-27 09:44:46 -0800

    make logger use memoized ivar reader

--- lib/reel/logger.rb
+++ lib/reel/logger.rb
@@ -2,20 +2,19 @@ require 'logger'
 
 module Reel
   module Logger
-    @logger = ::Logger.new STDERR
     module_function
-    
+
     def logger=(logger)
       @logger = logger
     end
-    
+
     def logger
-      @logger
+      @logger ||= ::Logger.new STDERR
     end
-    
-    def debug(msg); @logger.debug(msg); end
-    def info(msg);  @logger.info(msg);  end
-    def warn(msg);  @logger.warn(msg);  end
-    def error(msg); @logger.error(msg); end
+
+    def debug(msg); logger.debug(msg); end
+    def info(msg);  logger.info(msg);  end
+    def warn(msg);  logger.warn(msg);  end
+    def error(msg); logger.error(msg); end
   end
 end
